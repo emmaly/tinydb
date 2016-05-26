230ab42dd07a55166545c4274cc0e7d7aeb43ba9
Dusty Wilson <dusty@linux.com>
2016-05-25 21:36:29 -0700

    Key can contain slashes; bug fixes

--- something.go
+++ something.go
@@ -1,10 +1,12 @@
 package main
 
 import (
+	"errors"
 	"fmt"
 	"io/ioutil"
 	"log"
 	"net/http"
+	"strings"
 	"time"
 
 	"github.com/boltdb/bolt"
@@ -18,6 +20,9 @@ var validKeys = []string{
 
 var db *bolt.DB
 
+// ErrNoBucket means the requested bucket doesn't exist
+var ErrNoBucket = errors.New("bucket doesn't exist")
+
 func main() {
 	var err error
 	db, err = bolt.Open("tiny.db", 0600, nil)
@@ -28,7 +33,9 @@ func main() {
 
 	r := mux.NewRouter()
 	r.HandleFunc("/", authTest(homeHandler))
-	s := r.Path("/data/{bucket}/{key}").Subrouter()
+	r.HandleFunc("/favicon.ico", notFound)
+	r.HandleFunc("/robots.txt", notFound)
+	s := r.PathPrefix("/data/{bucket}/{key:.+}").Subrouter()
 	s.Methods("GET").HandlerFunc(authTest(keyGet))
 	s.Methods("POST", "PUT").HandlerFunc(authTest(keyPost))
 	s.Methods("DELETE").HandlerFunc(authTest(keyDelete))
@@ -41,6 +48,12 @@ func main() {
 
 func authTest(fxn func(http.ResponseWriter, *http.Request)) func(http.ResponseWriter, *http.Request) {
 	return func(w http.ResponseWriter, r *http.Request) {
+		v := mux.Vars(r)
+		if strings.Contains(v["key"], "::") {
+			log.Printf("INVALID REQUEST URL [%+v]\n", r.URL)
+			http.Error(w, "Invalid Request", http.StatusTeapot)
+			return
+		}
 		valid := false
 		for _, key := range validKeys {
 			if key == r.Header.Get("x-secret-squirrel") {
@@ -53,7 +66,7 @@ func authTest(fxn func(http.ResponseWriter, *http.Request)) func(http.ResponseWr
 		if valid {
 			fxn(w, r)
 		} else {
-			log.Println("AUTHTEST FAIL")
+			log.Printf("AUTHTEST FAIL [%+v]\n", r.URL)
 			http.Error(w, "Not Authorized", http.StatusTeapot)
 		}
 	}
@@ -77,17 +90,27 @@ func keyGet(w http.ResponseWriter, r *http.Request) {
 	log.Printf("[%s:%s] [%s/%s]\n", t, r.Method, v["bucket"], v["key"])
 	err := db.View(func(tx *bolt.Tx) error {
 		b := tx.Bucket([]byte(v["bucket"]))
-		mt := new(time.Time)
-		err := mt.UnmarshalBinary(b.Get([]byte(v["key"] + "/modified")))
-		if err != nil {
-			return err
+		if b == nil {
+			return ErrNoBucket
 		}
-		w.Header().Add("date", mt.Format(time.RFC1123))
+		mt := time.Time{}
+		mod := b.Get([]byte(v["key"] + "::modified"))
+		if mod != nil {
+			err := mt.UnmarshalBinary(mod)
+			if err != nil {
+				return err
+			}
+			w.Header().Add("date", mt.Format(time.RFC1123))
+		}
 		d := b.Get([]byte(v["key"]))
+		if d == nil {
+			http.Error(w, "Not Found", http.StatusNotFound)
+			return nil
+		}
 		var contentType string
 		if ct := r.Header.Get("x-content-type"); ct != "" {
 			contentType = ct
-		} else if ct := b.Get([]byte(v["key"] + "/content-type")); len(ct) != 0 {
+		} else if ct := b.Get([]byte(v["key"] + "::content-type")); len(ct) != 0 {
 			contentType = string(ct)
 		} else {
 			contentType = http.DetectContentType(d)
@@ -97,6 +120,10 @@ func keyGet(w http.ResponseWriter, r *http.Request) {
 		return nil
 	})
 	if err != nil {
+		if err == ErrNoBucket {
+			http.Error(w, "Not Found", http.StatusNotFound)
+			return
+		}
 		http.Error(w, err.Error(), http.StatusInternalServerError)
 		fmt.Fprintln(w, err)
 		return
@@ -117,15 +144,15 @@ func keyPost(w http.ResponseWriter, r *http.Request) {
 			return err
 		}
 		t, _ := time.Now().MarshalBinary()
-		b.Put([]byte(v["key"]+"/modified"), t)
+		b.Put([]byte(v["key"]+"::modified"), t)
 		var contentType string
 		if ct := r.Header.Get("x-content-type"); ct != "" {
 			contentType = ct
 		} else {
 			contentType = http.DetectContentType(d)
 		}
-		b.Put([]byte(v["key"]+"/length"), []byte(fmt.Sprintf("%d", len(d))))
-		b.Put([]byte(v["key"]+"/content-type"), []byte(contentType))
+		b.Put([]byte(v["key"]+"::length"), []byte(fmt.Sprintf("%d", len(d))))
+		b.Put([]byte(v["key"]+"::content-type"), []byte(contentType))
 		b.Put([]byte(v["key"]), d)
 		return nil
 	})
@@ -139,6 +166,7 @@ func keyPost(w http.ResponseWriter, r *http.Request) {
 }
 
 func keyDelete(w http.ResponseWriter, r *http.Request) {
+	t := "keyDelete"
 	v := mux.Vars(r)
 	err := db.Update(func(tx *bolt.Tx) error {
 		b, err := tx.CreateBucketIfNotExists([]byte(v["bucket"]))
@@ -146,9 +174,9 @@ func keyDelete(w http.ResponseWriter, r *http.Request) {
 			return err
 		}
 		b.Delete([]byte(v["key"]))
-		b.Delete([]byte(v["key"] + "/length"))
-		b.Delete([]byte(v["key"] + "/content-type"))
-		b.Delete([]byte(v["key"] + "/modified"))
+		b.Delete([]byte(v["key"] + "::length"))
+		b.Delete([]byte(v["key"] + "::content-type"))
+		b.Delete([]byte(v["key"] + "::modified"))
 		return nil
 	})
 	if err != nil {
@@ -156,38 +184,58 @@ func keyDelete(w http.ResponseWriter, r *http.Request) {
 		fmt.Fprintln(w, err)
 		return
 	}
-	http.Error(w, "", http.StatusOK)
+	log.Printf("[%s:%s] [%s/%s]\n", t, r.Method, v["bucket"], v["key"])
 }
 
 func keyHead(w http.ResponseWriter, r *http.Request) {
 	t := "keyHead"
 	v := mux.Vars(r)
 	log.Printf("[%s:%s] [%s/%s]\n", t, r.Method, v["bucket"], v["key"])
-	db.View(func(tx *bolt.Tx) error {
+	err := db.View(func(tx *bolt.Tx) error {
 		b := tx.Bucket([]byte(v["bucket"]))
-		mt := new(time.Time)
-		err := mt.UnmarshalBinary(b.Get([]byte(v["key"] + "/modified")))
-		if err != nil {
-			return err
+		if b == nil {
+			return ErrNoBucket
 		}
-		w.Header().Add("date", mt.Format(time.RFC1123))
+		mt := time.Time{}
+		mod := b.Get([]byte(v["key"] + "::modified"))
+		if mod != nil {
+			err := mt.UnmarshalBinary(mod)
+			if err != nil {
+				return err
+			}
+			w.Header().Add("date", mt.Format(time.RFC1123))
+		}
 		d := b.Get([]byte(v["key"]))
+		if d == nil {
+			http.Error(w, "Not Found", http.StatusNotFound)
+			return nil
+		}
 		var contentType string
 		if ct := r.Header.Get("x-content-type"); ct != "" {
 			contentType = ct
-		} else if ct := b.Get([]byte(v["key"] + "/content-type")); len(ct) != 0 {
+		} else if ct := b.Get([]byte(v["key"] + "::content-type")); len(ct) != 0 {
 			contentType = string(ct)
 		} else {
 			contentType = http.DetectContentType(d)
 		}
 		w.Header().Add("content-type", contentType)
-		if l := b.Get([]byte(v["key"] + "/length")); len(l) != 0 {
+		if l := b.Get([]byte(v["key"] + "::length")); len(l) != 0 {
 			w.Header().Add("content-length", string(l))
 		} else {
 			w.Header().Add("content-length", string(len(b.Get([]byte(v["key"])))))
 		}
+		//w.Write(d)
 		return nil
 	})
+	if err != nil {
+		if err == ErrNoBucket {
+			http.Error(w, "Not Found", http.StatusNotFound)
+			return
+		}
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		fmt.Fprintln(w, err)
+		return
+	}
 }
 
 func keyDefault(w http.ResponseWriter, r *http.Request) {
